6c8b4f9dbded96dee9cc9d915ea596c9c22b51aa
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2025-01-31 11:48:20 +0530

    Add raycasting functionality and instance color change on pointer move; update cube instancing logic

--- src/main.js
+++ src/main.js
@@ -12,6 +12,9 @@ import {
 	Fog,
 	MeshStandardMaterial,
 	Vector3,
+	Color,
+	Vector2,
+	Raycaster,
 } from "three";
 import "./style.css";
 
@@ -31,6 +34,12 @@ const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const raycaster = new Raycaster();
+const mouse = new Vector2(1, 1);
+
+const color = new Color();
+const white = new Color().setHex(0xffffff);
+
 const gridSize = 140;
 const planeGeometry = new PlaneGeometry(gridSize * 5, gridSize * 5);
 const planeMaterial = new MeshStandardMaterial({
@@ -41,34 +50,43 @@ plane.rotation.x = -(Math.PI / 2);
 
 const cubeGeometry = new BoxGeometry(1, 1, 1);
 const cubeMaterial = new MeshStandardMaterial({ color: 0xffffff });
-const cube = new InstancedMesh(
+const cubes = new InstancedMesh(
 	cubeGeometry,
 	cubeMaterial,
 	Math.pow(gridSize, 2)
 );
-cube.position.set(-(gridSize / 2), 0.5, -(gridSize / 2));
+cubes.position.set(-(gridSize / 2), 0.5, -(gridSize / 2));
 
 const ambientLight = new AmbientLight(0xffffff, 1);
 const directionalLight = new DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 10, 0);
 
 const spacing = 1.1;
+let index = 0;
 
-for (let index = 0; index < cube.count; index++) {
-	const row = Math.floor(index / gridSize);
-	const col = index % gridSize;
+for (let row = 0; row < gridSize; row++) {
+	for (let col = 0; col < gridSize; col++) {
+		const position = new Vector3(col * spacing, 0, row * spacing);
 
-	const position = new Vector3(col * spacing, 0, row * spacing);
+		const instanceMatrix = new Matrix4();
+		instanceMatrix.setPosition(position);
+		cubes.setMatrixAt(index, instanceMatrix);
+		cubes.setColorAt(index, white);
 
-	const instanceMatrix = new Matrix4();
-	instanceMatrix.setPosition(position);
-	cube.setMatrixAt(index, instanceMatrix);
+		index++;
+	}
 }
 
 scene.add(ambientLight);
 scene.add(directionalLight);
 scene.add(plane);
-scene.add(cube);
+scene.add(cubes);
+
+window.addEventListener("pointermove", (event) => {
+	event.preventDefault();
+	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
 
 window.addEventListener("resize", () => {
 	renderer.setSize(window.innerWidth, window.innerHeight);
@@ -77,6 +95,22 @@ window.addEventListener("resize", () => {
 });
 
 function animate() {
+	raycaster.setFromCamera(mouse, camera);
+
+	const intersection = raycaster.intersectObject(cubes);
+
+	if (intersection.length > 0) {
+		const instanceId = intersection[0].instanceId;
+
+		cubes.getColorAt(instanceId, color);
+
+		if (color.equals(white)) {
+			cubes.setColorAt(instanceId, color.setHex(Math.random() * 0xffffff));
+
+			cubes.instanceColor.needsUpdate = true;
+		}
+	}
+
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
 }
